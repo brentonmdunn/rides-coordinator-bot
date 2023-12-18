c74417c6c3114504e17ed7f90b474642e0e3ea59
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-12-18 11:05:34 -0800

    Clean up code

--- bot.py
+++ bot.py
@@ -2,26 +2,32 @@
 
 import os
 import random
-import copy
+from typing import List
+
 import discord
 from dotenv import load_dotenv
 
+# Environment variables from .env file
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 BOT_NAME = os.getenv('BOT_NAME')
-needs_ride = []
-prev_needs_ride = []
-drivers = []
-reacts = ['🥐', '🧁', '🍩', '🌋', '🦕', '🐸', '🐟', '🐻', '🦔']
+GUILD_ID = os.getenv('GUILD_ID')
+
+# Constants
 RIDES_MESSAGE: str = "React for rides."
+REACTS: List[str] = ['🥐', '🧁', '🍩', '🌋', '🦕', '🐸', '🐟', '🐻', '🦔']
+
+# Global variables
+needs_ride: List[str] = []
+drivers: List[str] = []
 current_reaction: int = 0
 message_id = None
 
 def run():
 
-    intents = discord.Intents.default()
+    intents: discord.Intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
+    client: discord.Client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
@@ -31,9 +37,11 @@ def run():
     @client.event
     async def on_message(message):
         """Sends message and puts first reaction"""
+
         global message_id
         global current_reaction
         global needs_ride
+
         # Makes sure that is not triggered by its own message
         if message.author == client.user:
             return
@@ -48,29 +56,16 @@ def run():
             await message.channel.send("```!send - sends ride message \n!get_reactions - lists users who have reacted```")
 
         if user_message == "!send":
-            # Clears list before each reaction 
-
-            prev_needs_ride = copy.deepcopy(needs_ride)
+            # Clears list before each reaction
             needs_ride = []
+
             sent_message = await message.channel.send(RIDES_MESSAGE)
             message_id = sent_message.id
+
             # Adds random reaction for ride
-            current_reaction = random.randint(0, len(reacts) - 1)
-            await sent_message.add_reaction(reacts[current_reaction])
+            current_reaction = random.randint(0, len(REACTS) - 1)
+            await sent_message.add_reaction(REACTS[current_reaction])
             print(current_reaction)
-
-        
-        if user_message == "!list":
-            if len(needs_ride) == 0:
-                await message.channel.send("List currently empty.")
-            else:
-                # needs_ride_username = ["@" + name for name in needs_ride]
-                # await message.channel.send(", ".join(needs_ride_username))
-                mentions = [f"<@{message.guild.get_member_named(name).id}>" for name in needs_ride]
-                await message.channel.send(" ".join(mentions))
-
-        if message.content == "!m_id":
-            await message.channel.send(str(message_id))
         
         if message.content == "!get_reactions":
             # Fetch the message for which you want to get reactions
@@ -78,7 +73,7 @@ def run():
                 await message.channel.send("Message has not sent yet.")
                 return
             
-            target_message = await message.channel.fetch_message(message_id)  # Replace message_id with the desired message ID
+            target_message = await message.channel.fetch_message(message_id)
 
             # Iterate through reactions and collect users who reacted
             reaction_users = set()
@@ -88,10 +83,7 @@ def run():
                         continue
                     reaction_users.add(user)
 
-            # Output the list of users who reacted
             users_list = ", ".join(str(user) for user in reaction_users)
             await message.channel.send(f"Users who reacted: {users_list}")
-            # mentions = [f"<@{message.guild.get_member_named(name).id}>" for name in reaction_users]
-            # await message.channel.send(" ".join(mentions))
 
     client.run(TOKEN)
