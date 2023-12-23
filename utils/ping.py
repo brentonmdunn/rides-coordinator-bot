f51220e1c4829b53aef9cef893324be4d6bb08fa
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-12-23 09:45:42 -0800

    Create get member method

--- utils/ping.py
+++ utils/ping.py
@@ -1,1 +1,4 @@
-
+import discord
+def get_member(guild_members: discord.utils.SequenceProxy, username: str) -> discord.member.Member:
+    """Returns ping-able member object."""
+    return discord.utils.get(guild_members, name=username)
