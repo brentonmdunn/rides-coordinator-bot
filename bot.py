75d1b9e02be4b9bee64fa403ed81e4a4b92a7d87
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-12-24 16:10:06 -0800

    Add documentation

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ from typing import List
 import discord
 from dotenv import load_dotenv
 
+import utils.ping as ping
+
 # Environment variables from .env file
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -15,16 +17,17 @@ BOT_NAME = os.getenv('BOT_NAME')
 # Constants
 RIDES_MESSAGE: str = "React for rides."
 REACTS: List[str] = ['🥐', '🧁', '🍩', '🌋', '🦕', '🐸', '🐟', '🐻', '🦔']
+ROLE_ID: int = 1188019586470256713
 
 # Global variables
 needs_ride: List[str] = []
 drivers: List[str] = []
 current_reaction: int = 0
-message_id = None
+message_id: int = None
 
 def run():
 
-    intents: discord.Intents = discord.Intents.default()
+    intents: discord.Intents = discord.Intents.all()
     intents.message_content = True
     client: discord.Client = discord.Client(intents=intents)
 
@@ -51,24 +54,47 @@ def run():
 
         print(f"{username} said: '{user_message}' ({channel})")
 
+        if user_message == "!test":
+
+            role = ping.get_role(message.guild, 1188019586470256713)
+            await message.channel.send(f"{role.mention}")
+
+            return
+            # for member in message.guild.members:
+            #     print(member)
+            # print("Test run")
+            gaggle_user = discord.utils.get(message.guild.members, name='brentond')  # Change 'gaggle' to the actual username
+            
+            user = ping.get_member(message.guild.members, 'brentond')
+            
+            print(type(message.guild.members))
+            if gaggle_user:
+                await message.channel.send(f"{user.mention}, someone mentioned 'Gaggle'!")
+            role = message.guild.get_role(1188019586470256713)
+            await message.channel.send(f"{role.mention}")
+
+
+
         if user_message == "!help":
             await message.channel.send("""```
                                        !send - sends ride message \n
                                        !get_reactions - lists users who have reacted
                                        ```""")
 
+
         if user_message == "!send":
             # Clears list before each reaction
             needs_ride = []
 
-            sent_message = await message.channel.send(RIDES_MESSAGE)
+            to_send: str = ping.create_message(ping.get_role(message.guild, ROLE_ID), RIDES_MESSAGE)
+            sent_message: discord.message.Message = await message.channel.send(to_send)
             message_id = sent_message.id
 
             # Adds random reaction for ride
             current_reaction = random.randint(0, len(REACTS) - 1)
             await sent_message.add_reaction(REACTS[current_reaction])
             print(current_reaction)
-        
+
         if message.content == "!get_reactions":
             # Fetch the message for which you want to get reactions
             if message_id is None:
@@ -87,5 +113,22 @@ def run():
 
             users_list = ", ".join(str(user) for user in reaction_users)
             await message.channel.send(f"Users who reacted: {users_list}")
+        
+        if message.content == "!ping":
+            if message_id is None:
+                await message.channel.send("Message has not sent yet.")
+                return
+            target_message = await message.channel.fetch_message(message_id)
+
+            reaction_users = set()
+            for reaction in target_message.reactions:
+                async for user in reaction.users():
+                    if str(user) == BOT_NAME:
+                        continue
+                    reaction_users.add(user)
+
+            users_list = ", ".join(ping.get_member(message.guild.members, str(user)).mention for user in reaction_users)
+            await message.channel.send(f"Users who reacted: {users_list}")
+
 
     client.run(TOKEN)
---
+++ commands/help.py
@@ -0,0 +1,2 @@
+async def execute(message):
+    await message.channel.send("""```!send - sends ride message \n!get_reactions - lists users who have reacted```""")
