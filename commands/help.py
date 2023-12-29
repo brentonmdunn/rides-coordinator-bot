102b6686d0ebb6c5ac31b453f76dc529da30d97f
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-12-28 16:55:34 -0800

    Reformat help

--- commands/help.py
+++ commands/help.py
@@ -6,8 +6,8 @@ import discord
 async def execute(message: discord.message.Message) -> None:
     """Lists commands that the user can run."""
     command_list = (
-        "!send - sends ride message"
-        "\n!get_reactions - lists users who have reacted"
-        "\n!group - groups users by location"
+        "`!send` - sends ride message"
+        "\n`!get_reactions` - lists users who have reacted"
+        "\n`!group` - groups users by location"
     )
-    await message.channel.send(f"```{command_list}```")
+    await message.channel.send(f"{command_list}")
