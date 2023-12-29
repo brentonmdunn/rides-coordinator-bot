2ae8cfe43cf582d21fe62d1d214f488f38cdcf45
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-12-29 13:59:47 -0800

    Depracate files b/c slash commands

---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+"""Contains constants"""
+
+from typing import List
+
+SEND_DESCRIPTION: str = "Sends the message for people to react to for rides."
+GROUP_DESCRIPTION: str = "Groups people by pickup location."
+HELP_DECRIPTION: str = "List of slash commands available."
+
+RIDES_MESSAGE: str = "React for rides."
+REACTS: List[str] = ['🥐', '🧁', '🍩', '🌋', '🦕', '🐸', '🐟', '🐻', '🦔']
+ROLE_ID: int = 1188019586470256713
+LOCATIONS_PATH = "locations.json"
