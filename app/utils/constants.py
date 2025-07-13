ced2d96b4413a2196da73046ded416f394878e45
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-07-13 15:40:00 -0700

    clean up constants

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,32 +1,3 @@
 """Contains constants"""
 
-SEND_DESCRIPTION: str = "Sends the message for people to react to for rides."
-GROUP_DESCRIPTION: str = "Groups people by pickup location."
-HELP_DESCRIPTION: str = "List of slash commands available."
-
-ADMIN_LIST_USER_INFO_DESCRIPTION: str = "Gets all user info or a named user (optional param)."
-ADMIN_GET_REACTION_USERS_DESCRIPTION: str = "Gets list of users who reacted to message."
-ADMIN_HELP_DESCRIPTION: str = "Slash commands available for admins."
-
-RIDES_MESSAGE1: str = "React if you want a ride to class this Sunday (leave around 8:40)"
-RIDES_MESSAGE2: str = "React if you want a ride to Sunday Service (leave around 10:10)"
-
-REACTS: list[str] = ["🥐", "🧁", "🍩", "🌋", "🦕", "🐸", "🐟", "🐻", "🦔"]
-# ROLE_ID: int = 1188019586470256713
-ROLE_ID = 940467850261450752
-
-
-AUTHORIZED_ADMIN: list[str] = ["brentond", "kendruh."]
-
-CAMPUS: list[str] = [
-    "Eighth",
-    "Revelle",
-    "Muir",
-    "Sixth",
-    "Marshall",
-    "ERC",
-    "Seventh",
-    "Warren",
-]
-
 GUILD_ID = 916817752918982716
