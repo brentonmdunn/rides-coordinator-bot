2b1ddcdf34ee7a4c2f8946c208291f340076dd61
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-07-21 20:24:06 -0700

    feat: Add name parsing

--- app/utils/parsing.py
+++ app/utils/parsing.py
@@ -23,3 +23,33 @@ def parse_discord_username(username: str) -> str:
     """Returns username without @ symbol."""
     username = username.lower().strip()
     return username if "@" not in username else username[1:]
+
+
+def get_first_name(name: str) -> str:
+    """
+    Returns first name of person. Works for both "fname" and "fname ... lname" formats.
+
+    Args:
+        name: Name to parse.
+
+    Returns:
+        First name of person.
+    """
+    return name.split()[0]
+
+
+def get_last_name(name: str) -> str | None:
+    """
+    Returns the last name of the person.
+
+    Args:
+        name: Name to parse.
+
+    Returns:
+        Last name of person. Returns `None` if `name="fname"`, returns `"mname lname"`
+        if `name="fname mname lname"`.
+    """
+    name_parts = name.split()
+    if len(name_parts) > 1:
+        return " ".join(name_parts[1:])
+    return None
