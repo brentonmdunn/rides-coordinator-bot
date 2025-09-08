15f64b66b0646d62294358e1132371873a945d7c
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-09-08 11:15:43 -0700

    feat: Add schema for rides user

--- app/core/schemas.py
+++ app/core/schemas.py
@@ -1,6 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from app.core.enums import PickupLocations
 
 class LocationQuery(BaseModel):
     start_location: PickupLocations
     end_location: PickupLocations
+
+class RidesUser(BaseModel):
+    name: str
+    username: str
+    location: PickupLocations
+
+    @field_validator('username', mode='before')
+    def add_at_symbol(cls, v: str) -> str:
+        if not v.startswith('@'):
+            return f'@{v}'
+        return v
