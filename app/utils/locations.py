164a908712b2833d5f42caafe6fa7689c72b5441
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-09-07 23:31:15 -0700

    feat: Add function to find time btwn locations

--- app/utils/locations.py
+++ app/utils/locations.py
@@ -1,3 +1,4 @@
+from app.core.schemas import LocationQuery
 
 LOCATIONS_MATRIX = {
     "Muir": [("Sixth", 2), ("Eighth", 3)],
@@ -9,3 +10,18 @@ LOCATIONS_MATRIX = {
     "Innovation": [("Warren", 2), ("Rita", 7)],
     "Eighth": [("Rita", 4), ("Muir", 3)]
 }
+
+def lookup_time(query: LocationQuery) -> int:
+    """
+    Looks up the travel time between two locations using a Pydantic model for input.
+
+    Args:
+        query: A LocationQuery model containing the start and end locations.
+
+    Returns:
+        The travel time as an integer, or raises an exception if the path is not found.
+    """
+    # Use the value of the enum member to access the dictionary
+    for neighbor, time in LOCATIONS_MATRIX[query.start_location.value]:
+        if neighbor == query.end_location.value:
+            return time
