7cb5e696bc5c8be4a6596deaec65df7b437af25f
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-09-07 19:34:06 -0700

    feat: Create locations matrix

---
+++ app/utils/locations.py
@@ -0,0 +1,11 @@
+
+LOCATIONS_MATRIX = {
+    "Muir": [("Sixth", 2), ("Eighth", 3)],
+    "Sixth": [("Muir", 2), ("ERC", 2)],
+    "ERC": [("Sixth", 2), ("Seventh", 2)],
+    "Seventh": [("ERC", 2), ("Warren", 4)],
+    "Warren": [("Seventh", 4), ("Rita", 8), ("Innovation", 2)],
+    "Rita": [("Warren", 8), ("Innovation", 7), ("Eighth", 4)],
+    "Innovation": [("Warren", 2), ("Rita", 7)],
+    "Eighth": [("Rita", 4), ("Muir", 3)]
+}
