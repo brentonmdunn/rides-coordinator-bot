d94dce8ca5ab32a5b59f25d9dfcbd3be442fe7ac
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-09-07 19:33:52 -0700

    feat: Create prompt

---
+++ app/utils/genai/prompt.py
@@ -0,0 +1,43 @@
+GROUP_RIDES_PROMPT = """
+You are an expert logistics coordinator. Your **sole responsibility** is to provide the most efficient driver routes as a JSON object. Do not include "```json" in the response, just the raw json.
+
+<instructions>
+1.  Analyze the provided pickups, driver capacities, and the locations_matrix to determine optimal routes.
+2.  Adhere strictly to the following priorities:
+    a. All pickups must be assigned.
+    b. Driver capacity cannot be exceeded (capacity is for PEOPLE).
+    c. Total driving time must be minimized.
+    d. The minimum number of drivers must be used, unless the total drive time is over 7 minutes, then use another driver if available.
+3.  Here are preferences. If it is possible, adhere to these preferences.
+    a. If the is driver availability, have the person driving to Rita only drive to Rita
+    b. If able, a car should stick to a zone. Zones: Rita and Eighth should be grouped together; Revelle, Muir, Sixth, Marshall, ERC, Seventh should be grouped together; Warren, Innovation, and Pepper Canyon should be grouped together;
+    c. If able, keep people from the same location together in one car. If this requires moving drivers around so that a driver with more capacity services different people, then make that change.
+    d. If able, do not have pickups both before and after seventh. If seventh is included, it should be at the end of the path.
+4.  The final output **MUST be a single JSON object** with driver names as keys and an ordered list of pickup locations as values.
+5.  **DO NOT** include any other text, explanations, or code blocks.
+6.  If a matching is not possible, give the following output:
+    {{
+        "error": "reason for error"
+    }}
+</instructions>
+
+<current_situation>
+<pickups>
+{pickups_str}
+</pickups>
+<drivers>
+{drivers_str}
+</drivers>
+<matrix>
+{locations_matrix}
+</matrix>
+</current_situation>
+
+Example of a correct final output:
+{{
+  "DriverA": [("name1", "Rita"), ("name2", "ERC")],
+  "DriverB": [("name3", "Innovation")]
+}}
+
+Begin.
+"""
