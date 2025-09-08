faa99cdf65dfe02605c80711d3b97cf8da4796f9
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-09-08 16:54:14 -0700

    feat: Create schema for `RidesUsers`

--- app/core/schemas.py
+++ app/core/schemas.py
@@ -5,13 +5,22 @@ class LocationQuery(BaseModel):
     start_location: PickupLocations
     end_location: PickupLocations
 
-class RidesUser(BaseModel):
+class Identity(BaseModel):
     name: str
     username: str
-    location: PickupLocations
 
     @field_validator('username', mode='before')
-    def add_at_symbol(cls, v: str) -> str:
+    def add_at_symbol(cls, v: str) -> str: # noqa: N805
         if not v.startswith('@'):
             return f'@{v}'
         return v
+
+
+class RidesUser(BaseModel):
+    """
+    - identity: Identity
+    - location: PickupLocations
+    """
+    identity: Identity
+    location: PickupLocations
+
