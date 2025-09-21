09ad55fafbd32e9bcabff171860145817f2fccf7
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-09-21 09:15:20 -0700

    feat: More emojis for driver cmd

--- app/cogs/ask_drivers.py
+++ app/cogs/ask_drivers.py
@@ -1,10 +1,19 @@
 """cogs/ask_drivers.py"""
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
-from app.core.enums import ChannelIds, FeatureFlagNames, RoleIds
-from app.utils.channel_whitelist import BOT_TESTING_CHANNELS, cmd_is_allowed
+from app.core.enums import (
+    ChannelIds,
+    DaysOfWeek,
+    FeatureFlagNames,
+    RoleIds,
+)
+from app.utils.channel_whitelist import (
+    BOT_TESTING_CHANNELS,
+    cmd_is_allowed,
+)
 from app.utils.checks import feature_flag_enabled
 from app.utils.format_message import ping_role_with_message
 
@@ -13,12 +22,25 @@ class AskDrivers(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    async def day_autocomplete(
+        self,
+        interaction: discord.Interaction,
+        current: str,
+    ) -> list[app_commands.Choice[str]]:
+        days = [DaysOfWeek.SUNDAY, DaysOfWeek.FRIDAY]
+        return [
+            app_commands.Choice(name=day, value=day)
+            for day in days
+            if current.lower() in day.lower()
+        ]
+
     @discord.app_commands.command(
         name="ask-drivers",
         description="Pings drivers to see who is available.",
     )
+    @app_commands.autocomplete(day=day_autocomplete)
     @feature_flag_enabled(FeatureFlagNames.BOT)
-    async def ask_drivers(self, interaction: discord.Interaction, message: str) -> None:
+    async def ask_drivers(self, interaction: discord.Interaction, day: str, message: str) -> None:
         """Pings the driver role with a custom message."""
         if not await cmd_is_allowed(
             interaction,
@@ -38,7 +60,10 @@ class AskDrivers(commands.Cog):
         # Fetch the original response
         sent_message = await interaction.original_response()
 
-        reactions = ["👍", "❌", "➡️", "⬅️", "💩"]
+        if day == DaysOfWeek.SUNDAY:
+            reactions = ["🍔", "🏠", "🔄", "❌", "➡️", "⬅️", "💩"]
+        else:  # Friday
+            reactions = ["👍", "❌", "➡️", "⬅️", "💩"]
         for emoji in reactions:
             await sent_message.add_reaction(emoji)
 
