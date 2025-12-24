fa92d36eff30c870155c37be9fc1fcdd286c713c
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2025-12-24 11:48:44 -0800

    fix: Google maps links

--- backend/bot/utils/constants.py
+++ backend/bot/utils/constants.py
@@ -7,15 +7,15 @@ GUILD_ID = 916817752918982716
 LSCC_DAYS = [DaysOfWeek.FRIDAY, DaysOfWeek.SUNDAY]
 
 MAP_LINKS = {
-    PickupLocations.SIXTH: "https://maps.bot.goo.gl/z8cffnYwLi1sgYcf8",
-    PickupLocations.SEVENTH: "https://maps.bot.goo.gl/1zKQiGKH6ecq1qzS8",
-    PickupLocations.MARSHALL: "https://maps.bot.goo.gl/1NT4Q65udUvuNX7aA",
-    PickupLocations.ERC: "https://maps.bot.goo.gl/dqgzKGS8DsUgLkw17",
-    PickupLocations.MUIR: "https://maps.bot.goo.gl/qxABq7sEEQsz6Pth9",
-    PickupLocations.EIGHTH: "https://maps.bot.goo.gl/RySbnmJGZ7zKujgq7",
-    PickupLocations.INNOVATION: "https://maps.bot.goo.gl/7tDt4mT5SkPkJbRh8",
-    PickupLocations.RITA: "https://maps.bot.goo.gl/qcuCR5q6Tx2EEn9c9",
-    PickupLocations.WARREN_EQL: "https://maps.bot.goo.gl/b4vLo5ZCGdZXEoni8",
-    PickupLocations.WARREN_JST: "https://maps.bot.goo.gl/h5LJCGhvBUbpmkmL7",
-    PickupLocations.GEISEL_LOOP: "https://maps.bot.goo.gl/nEtXGDLwGeAArNtPA",
+    PickupLocations.SIXTH: "https://maps.app.goo.gl/z8cffnYwLi1sgYcf8",
+    PickupLocations.SEVENTH: "https://maps.app.goo.gl/1zKQiGKH6ecq1qzS8",
+    PickupLocations.MARSHALL: "https://maps.app.goo.gl/1NT4Q65udUvuNX7aA",
+    PickupLocations.ERC: "https://maps.app.goo.gl/dqgzKGS8DsUgLkw17",
+    PickupLocations.MUIR: "https://maps.app.goo.gl/qxABq7sEEQsz6Pth9",
+    PickupLocations.EIGHTH: "https://maps.app.goo.gl/RySbnmJGZ7zKujgq7",
+    PickupLocations.INNOVATION: "https://maps.app.goo.gl/7tDt4mT5SkPkJbRh8",
+    PickupLocations.RITA: "https://maps.app.goo.gl/qcuCR5q6Tx2EEn9c9",
+    PickupLocations.WARREN_EQL: "https://maps.app.goo.gl/b4vLo5ZCGdZXEoni8",
+    PickupLocations.WARREN_JST: "https://maps.app.goo.gl/h5LJCGhvBUbpmkmL7",
+    PickupLocations.GEISEL_LOOP: "https://maps.app.goo.gl/nEtXGDLwGeAArNtPA",
 }
