fab3ac35b803ddf8968f23ec0697bb271b24018c
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2026-02-08 16:15:58 -0800

    feat: Add skeleton for new page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
-import App from './App.tsx'
+import Home from './pages/Home.tsx'
+import Learn from './pages/Learn.tsx'
 import { ThemeProvider } from "./components/theme-provider"
 
 const queryClient = new QueryClient()
@@ -11,7 +13,12 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/learn" element={<Learn />} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
