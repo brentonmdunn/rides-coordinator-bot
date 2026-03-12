bf654ccc8979b6b355ce1c3eaeab478f8bd38256
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2026-03-12 11:27:10 -0700

    feat: Pin all pickup locations

--- frontend/src/pages/RouteBuilder.tsx
+++ frontend/src/pages/RouteBuilder.tsx
@@ -1,11 +1,22 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { useQuery } from '@tanstack/react-query'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { UCSD_CENTER, setupLeafletIcons } from '../components/MapShared'
+import { apiFetch } from '../lib/api'
+import type { PickupLocationsResponse } from '../types'
 
 // Fix default marker icon
 setupLeafletIcons()
 
 export default function RouteBuilder() {
+    const { data: locationsData } = useQuery<PickupLocationsResponse>({
+        queryKey: ['pickup-locations'],
+        queryFn: async () => {
+            const res = await apiFetch('/api/pickup-locations')
+            return res.json()
+        },
+    })
+
     return (
         <div className="h-screen w-full relative">
             <MapContainer
@@ -17,6 +28,17 @@ export default function RouteBuilder() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                
+                {locationsData?.locations.map((loc) => {
+                    const coords = locationsData.coordinates[loc.value]
+                    if (!coords) return null
+                    
+                    return (
+                        <Marker key={loc.key} position={[coords.lat, coords.lng]}>
+                            <Popup>{loc.value}</Popup>
+                        </Marker>
+                    )
+                })}
             </MapContainer>
         </div>
     )
