1d977c9bbd7809c10b93514189c5ce4638b59443
bmdunn <brentondunn@yahoo.com>
2026-04-25 17:19:06 +0000

    feat: add React error boundary to prevent white-screen crashes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
+import ErrorBoundary from './components/ErrorBoundary.tsx'
 import Home from './pages/Home.tsx'
 import Learn from './pages/Learn.tsx'
 import { ThemeProvider } from "./components/theme-provider"
@@ -13,12 +14,14 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/learn" element={<Learn />} />
-          </Routes>
-        </BrowserRouter>
+        <ErrorBoundary>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/learn" element={<Learn />} />
+            </Routes>
+          </BrowserRouter>
+        </ErrorBoundary>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
