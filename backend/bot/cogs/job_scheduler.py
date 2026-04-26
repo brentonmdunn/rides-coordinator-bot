a1e1990217dd86232cb3decde757f028259d129b
bmdunn <brentondunn@yahoo.com>
2026-04-26 08:32:04 +0000

    refactor: remove commented-out job registrations from JobScheduler

--- backend/bot/cogs/job_scheduler.py
+++ backend/bot/cogs/job_scheduler.py
@@ -5,8 +5,6 @@ from apscheduler.triggers.cron import CronTrigger
 from discord.ext import commands
 
 from bot.core.enums import ChannelIds
-
-# from jobs_disabled.retreat_sync_roles import run_csv_job
 from bot.jobs.ask_rides import (
     run_ask_rides_all,
     run_periodic_cache_warming,
@@ -22,14 +20,6 @@ class JobScheduler(commands.Cog):
         self.bot = bot
         self.scheduler = AsyncIOScheduler()
 
-        # # Register all jobs
-        # self.scheduler.add_job(
-        #     run_csv_job,
-        #     CronTrigger(hour=21, minute=00),
-        #     id="csv_job",
-        #     args=[bot],
-        # )
-
         self.scheduler.add_job(
             run_ask_rides_all,
             CronTrigger(day_of_week="wed", hour=12, minute=0),
@@ -44,45 +34,11 @@ class JobScheduler(commands.Cog):
             args=[bot],
         )
 
-        # self.scheduler.add_job(
-        #     run_ask_rides_fri,
-        #     CronTrigger(day_of_week="wed", hour=12, minute=0),
-        #     id="run_ask_rides_fri",
-        #     args=[bot],
-        # )
-
-        # self.scheduler.add_job(
-        #     run_ask_rides_wed,
-        #     CronTrigger(day_of_week="mon", hour=16, minute=0),
-        #     id="run_ask_rides_wed",
-        #     args=[bot],
-        # )
-
-        # self.scheduler.add_job(
-        #     run_ask_rides_sun,
-        #     CronTrigger(day_of_week="fri", hour=23, minute=19),
-        #     id="run_ask_rides_sun",
-        #     args=[bot],
-        # )
-
-        # self.scheduler.add_job(
-        #     run_ask_rides_sun_class,
-        #     CronTrigger(day_of_week="wed", hour=16, minute=2),
-        #     id="run_ask_rides_sun_class",
-        #     args=[bot],
-        # )
-
         self.scheduler.add_job(
             sync_rides_locations,
             CronTrigger(hour=3, minute=0),
             id="sync_rides_locations",
         )
-
-        # self.scheduler.add_job(
-        #     delete_past_pickups,
-        #     CronTrigger(day_of_week="mon", hour=3, minute=0),
-        #     id="delete_past_pickups",
-        # )
 
         self.scheduler.start()
 
