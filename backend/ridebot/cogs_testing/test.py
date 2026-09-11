160c36def81eee59ac89abf2b790fd7225d2e6d2
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2026-09-11 13:29:59 -1000

    feat: rename signup button

--- backend/ridebot/cogs_testing/test.py
+++ backend/ridebot/cogs_testing/test.py
@@ -28,7 +28,7 @@ class TestCog(commands.Cog):
     @bot_enabled
     async def test_register(self, interaction: discord.Interaction):
         await interaction.response.send_message(
-            "Tap **Rides Form** to test the roster form.", view=RegistrationView()
+            "Tap **Sign up for rides** to test the roster form.", view=RegistrationView()
         )
 
 
